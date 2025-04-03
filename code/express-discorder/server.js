3f3360af641f7c8dff2a9c707a00e048ec67cf83
David Chong <david.chong.ubc@gmail.com>
2025-04-03 16:25:41 -0700

    moved things over to .env

--- code/express-discorder/server.js
+++ code/express-discorder/server.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const express = require("express");
+const basicAuth = require('express-basic-auth');
+
 const axios = require("axios").default;
 const bodyParser = require("body-parser");
 
@@ -24,21 +26,50 @@ function logToFile(message, logFilePath) {
   });
 }
 
+function showLog(res, filename) {
+  try {
+    const logContent = fs.readFileSync(filename, 'utf-8');
+    res.send(`<pre>${logContent}</pre>`);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      res.status(404).send('File not found');
+    } else {
+      console.error(error);
+      res.status(500).send('Server error');
+    }
+  }
+}
+
+const users = { [process.env.USERAUTH]: process.env.USERPASS };
+
+const auth = basicAuth({
+  users: users,
+  challenge: true, // This will cause the browser to show a login prompt
+});
 
 app.use(express.json());
 app.use(bodyParser.json()); // To parse application/json bodies
 
 app.get("/", (req, res) => res.send(`
   <html>
-    <head><title>Success!</title></head>
+    <head><title>Hooks!</title></head>
     <body>
-      <h1>You did it!</h1>
-      <img src="https://media.giphy.com/media/XreQmk7ETCak0/giphy.gif" alt="Cool kid doing thumbs up" />
+      <h1>Hooks!</h1>
+      <img src="https://i.pinimg.com/736x/60/2d/0f/602d0f4ed18cd92e08bb45307dbdf15d.jpg" alt="Doraemon" />
     </body>
   </html>
 `));
 
-app.post('/sms/8LvraOfRY5cbM3S8EDjhQu4KBeO1JkCv', (req, res) => {
+
+app.get(process.env.USER_A_ENDPOINT, auth, (req, res) => {
+  showLog(res, process.env.USER_A_FILE);
+});
+
+app.get(process.env.USER_B_ENDPOINT, auth, (req, res) => {
+  showLog(res, process.env.USER_B_FILE);
+});
+
+app.post(process.env.WEBHOOK_URL_A, (req, res) => {
   const data = req.body.data;
   
   if (!data) {
@@ -52,14 +83,14 @@ app.post('/sms/8LvraOfRY5cbM3S8EDjhQu4KBeO1JkCv', (req, res) => {
   const myMessage = `📩 Received SMS from ${from} to ${to}: ${message}`;
 
   console.log(myMessage);
-  const logFilePath = path.join(__dirname, 'moof.log');
+  const logFilePath = path.join(__dirname, process.env.USER_A_FILE);
 
   logToFile(myMessage, logFilePath);
 
   res.sendStatus(200); // Acknowledge receipt to Telnyx
 });
 
-app.post('/sms/L7FwMP0QnRZ2J3d7MpP11hpA3IKeHq1H', (req, res) => {
+app.post(process.env.WEBHOOK_URL_B, (req, res) => {
   const data = req.body.data;
 
   if (!data) {
@@ -73,7 +104,7 @@ app.post('/sms/L7FwMP0QnRZ2J3d7MpP11hpA3IKeHq1H', (req, res) => {
   const myMessage = `📩 Received SMS from ${from} to ${to}: ${message}`;
 
   console.log(myMessage);
-  const logFilePath = path.join(__dirname, 'toof.log');
+  const logFilePath = path.join(__dirname, process.env.USER_B_FILE);
 
   logToFile(myMessage, logFilePath);
 
