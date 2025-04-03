06dd1096626395ac86401e4a9c3b386d231307e2
David Chong <david.chong.ubc@gmail.com>
2025-04-03 15:14:35 -0700

    silly webhooks

--- code/express-discorder/server.js
+++ code/express-discorder/server.js
@@ -1,11 +1,32 @@
 require("dotenv").config();
 const express = require("express");
 const axios = require("axios").default;
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
 
+const fs = require('fs');
+const path = require('path');
+const { DateTime } = require('luxon');
+
+
+// Function to write to the log file
+function logToFile(message, logFilePath) {
+
+  const pstDate = DateTime.now().setZone('America/Los_Angeles').toISO();
+  const logMessage = `${pstDate} - ${message}\n`;
+
+  fs.appendFile(logFilePath, logMessage, (err) => {
+    if (err) {
+      console.error('Error writing to log file:', err);
+    }
+  });
+}
+
+
 app.use(express.json());
+app.use(bodyParser.json()); // To parse application/json bodies
 
 app.get("/", (req, res) => res.send(`
   <html>
@@ -17,26 +38,48 @@ app.get("/", (req, res) => res.send(`
   </html>
 `));
 
-app.post("/github", (req, res) => {
-  const content = ":wave: Hi mom!";
-  const avatarUrl = "https://media.giphy.com/media/3o7TKSjRrfIPjeiVyM/giphy.gif";
-  axios
-    .post(process.env.DISCORD_WEBHOOK_URL, {
-      content: content,
-      embeds: [
-        {
-          image: {
-            url: avatarUrl,
-          },
-        },
-      ],
-    })
-    .then((discordResponse) => {
-      console.log("Success!");
-      res.status(204).send();
-    })
-    .catch((err) => console.error(`Error sending to Discord: ${err}`));
+app.post('/sms/8LvraOfRY5cbM3S8EDjhQu4KBeO1JkCv', (req, res) => {
+  const data = req.body.data;
+  
+  if (!data) {
+    return res.status(400).send('Missing data');
+  }
+
+  const from = data.payload.from.phone_number;
+  const to = data.payload.to[0].phone_number;
+  const message = data.payload.text;
+
+  const myMessage = `📩 Received SMS from ${from} to ${to}: ${message}`;
+
+  console.log(myMessage);
+  const logFilePath = path.join(__dirname, 'moof.log');
+
+  logToFile(myMessage, logFilePath);
+
+  res.sendStatus(200); // Acknowledge receipt to Telnyx
 });
+
+app.post('/sms/L7FwMP0QnRZ2J3d7MpP11hpA3IKeHq1H', (req, res) => {
+  const data = req.body.data;
+
+  if (!data) {
+    return res.status(400).send('Missing data');
+  }
+
+  const from = data.payload.from.phone_number;
+  const to = data.payload.to[0].phone_number;
+  const message = data.payload.text;
+
+  const myMessage = `📩 Received SMS from ${from} to ${to}: ${message}`;
+
+  console.log(myMessage);
+  const logFilePath = path.join(__dirname, 'toof.log');
+
+  logToFile(myMessage, logFilePath);
+
+  res.sendStatus(200); // Acknowledge receipt to Telnyx
+});
+
 
 app.use((error, req, res, next) => {
   res.status(500)
